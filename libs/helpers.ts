c9be51082081abd8b5937ce1bd9acd08cf1cea60
Botirjon Shokirov <botirjon@radcode.co>
2023-06-04 20:49:04 +0200

    fixed problems

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,14 +1,14 @@
 import { Price } from "@/types";
 
-export const getUrl = () => {
+export const getURL = () => {
   let url =
-    process.env.NEXT_PUBLC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/";
-
+  // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`;
+  // Make sure to including trailing `/`.
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-
   return url;
 };
 
@@ -19,7 +19,8 @@ export const postData = async ({
   url: string;
   data?: { price: Price };
 }) => {
-  console.log("POST REQUEST", url, data);
+  console.log("posting,", url, data);
+
   const res: Response = await fetch(url, {
     method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
@@ -28,16 +29,16 @@ export const postData = async ({
   });
 
   if (!res.ok) {
-    console.log("Error in POST", { url, data, res });
+    console.log("Error in postData", { url, data, res });
 
-    throw new Error(res.statusText);
+    throw Error(res.statusText);
   }
 
   return res.json();
 };
 
 export const toDateTime = (secs: number) => {
-  var t = new Date("1970-01-01T00:30:00Z");
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
   t.setSeconds(secs);
   return t;
 };
